fb8df407db97f2a00878fa5247bf67c9bee1007e
Rahul_kr_singh <148681428+rahul-7071@users.noreply.github.com>
2023-11-12 08:13:03 +0530

    Add files via upload
    chocolate problem solution

---
+++ chocolate.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+int money,choco=0;
+cout<<"Enter how much money do you Have:";
+cin>>money;
+while (money!=0)
+{
+    choco+=money;
+    money/=3;
+
+}
+if (money==0)
+{
+   cout<<"Total No of chocolate you will get : "<<choco<<endl;
+
+}
+else if (money==1||money==2)
+   cout<<"Total No of chocolate you will get : "<<(choco)+1<<endl;
+{
+    /* code */
+}
+
+
+ return 0;
+}
+
+
